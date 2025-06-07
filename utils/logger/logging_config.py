e0f01906e4ed2b15a1827ad6a7d43bd19c856b36
NavidAdib98 <NavidAdib98@gmail.com>
2025-06-07 15:05:24 +0330

    adding docker-comose and docker file

--- utils/logger/logging_config.py
+++ utils/logger/logging_config.py
@@ -6,6 +6,9 @@ config_logging_filename = os.getenv("LOGGING_FILE", "./app.log")
 config_logging_level = os.getenv("LOGGING_LEVEL", "INFO")
 # ------- --------------------- ---------
 
+log_dir = os.path.dirname(config_logging_filename)
+if log_dir and not os.path.exists(log_dir):
+    os.makedirs(log_dir)
 
 logging.basicConfig(
     level=getattr(logging,config_logging_level),
